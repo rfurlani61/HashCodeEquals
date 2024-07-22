90784d57331e0b1a7d8740203e4cae7a2a5684c3
rfurlani <furlani.roberto@gmail.com>
2024-07-22 10:57:51 -0300

    Set command JAva language

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,50 @@
 package application;
 
-import entities.Client;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Client c1 = new Client("Maria", "maria@gmail.com");
-		Client c2 = new Client("Maria", "maria@gmail.com");
+		Set<String> set = new HashSet<>();
+		Set<String> set1 = new TreeSet<>();
+		Set<String> set2 = new LinkedHashSet<>();
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Tablet");
+		set1.add("TV");
+		set1.add("Notebook");
+		set1.add("Tablet");
+		set2.add("TV");
+		set2.add("Notebook");
+		set2.add("Tablet");
+		System.out.println(set.contains("Notebook"));
+		for (String p : set) {
+			System.out.println(p);
+		}
+		System.out.println(set1.contains("TV"));
+		for (String p : set1) {
+			System.out.println(p);
+		}
+		System.out.println(set2.contains("Tablet"));
+		for (String p : set2) {
+			System.out.println(p);
+		}
 		
-		System.out.println(c1.hashCode()); // -602288624
-		System.out.println(c2.hashCode()); // -602288624
-		System.out.println(c1.equals(c2)); // true
-		System.out.println(c1 == c2);      
-		// false => como foram criados 2 objs diferentes em 2 posições de memoria diferentes na comparação retorna que os objetos não são iguais por estarem em 
-		// posições de referencia de memória distintas.
-		
-		
-		String s1 = "Test";
-		String s2 = "Test";
-		System.out.println(s1 == s2); // true funciona porque estamos lidando com literais
-		String ss1 = new String("Test");
-		String ss2 = new String("Test");
-		System.out.println(ss1 == ss2); // false => ao fazeermos isso, estamos criando uma nova referencia de memória e por isso dará falso.
-		
+		set.remove("Tablet");
+		for (String p : set) {
+			System.out.println(p);
+		}
+		set1.removeIf(x -> x.length() > 3);
+		for (String p : set1) {
+			System.out.println(p);
+		}
+		set2.removeIf(x -> x.charAt(0) == 'T');
+		for (String p : set2) {
+			System.out.println(p);
+		}
 	}
 
 }
